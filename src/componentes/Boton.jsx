0d2374fea55cc03fc2eb7b9bb6979ced7148c68c
Frank Reyes <freyes081@gmail.com>
2023-05-29 14:01:32 -0400

    first commit

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/Boton.css";
+
+const Boton = ({ texto, esBotondeClic, manejarClic }) => {
+  return (
+    <button
+      className={esBotondeClic ? "boton-clic" : "boton-reiniciar"}
+      onClick={manejarClic}
+    >
+      {texto}
+    </button>
+  );
+};
+
+export default Boton;
